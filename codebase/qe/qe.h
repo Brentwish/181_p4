815635f4617a22e484962e95bd84c48f6a394f7a
Jaisal Singh <jsingh@pulsesecure.net>
2019-06-05 22:55:37 -0700

    Makefile fix

--- codebase/qe/qe.h
+++ codebase/qe/qe.h
@@ -230,12 +230,12 @@ class INLJoin : public Iterator {
         INLJoin(Iterator *leftIn,           // Iterator of input R
                IndexScan *rightIn,          // IndexScan Iterator of input S
                const Condition &condition   // Join condition
-        ){};
-        ~INLJoin(){};
+        );
+        ~INLJoin();
 
-        RC getNextTuple(void *data){return QE_EOF;};
+        RC getNextTuple(void *data);
         // For attribute in vector<Attribute>, name it as rel.attr
-        void getAttributes(vector<Attribute> &attrs) const{};
+        void getAttributes(vector<Attribute> &attrs) const;
     private:
         Iterator *leftRelation; // iterator for the left table
         IndexScan *rightIndex; // iterator for the right index
